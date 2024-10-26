a3a4db13a9821a344d3681f4de4b7b2030c13ba8
ujjwal <uzzwal7505@gmail.com>
2024-10-26 13:55:01 +0530

    checking something

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.post('/api/message', async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
